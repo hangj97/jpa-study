81a353a8a637d0f088fc29cb590efadcc46db857
hangj97 <gksruwls@naver.com>
2023-02-17 05:03:52 +0900

    챕터2 예제 실습

--- ch02-jpa-start1/src/main/java/jpabook/start/Member.java
+++ ch02-jpa-start1/src/main/java/jpabook/start/Member.java
@@ -1,22 +1,22 @@
 package jpabook.start;
 
-import javax.persistence.*;  //**
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-/**
- * User: HolyEyE
- * Date: 13. 5. 24. Time: 오후 7:43
- */
-@Entity
-@Table(name="MEMBER")
+@Entity                     // 해당 어노테이션이 붙어있는 클래스를 테이블과 매핑한다고 JPA에게 알림.
+@Table(name = "MEMBER")     // 엔티티 클래스에 매핑할 테이블 정보를 알려줌.
 public class Member {
 
-    @Id
-    @Column(name = "ID")
+    @Id                     // 기본 키 매핑 -> id 필드를 테이블의 id 기본 키 ㅋ러럼에 매핑 식별자 필드
+    @Column(name = "ID")    // 필드를 컬럼에 매용
     private String id;
 
     @Column(name = "NAME")
     private String username;
 
+    // 매핑 정보가 없는 필드
     private Integer age;
 
     public String getId() {
@@ -31,8 +31,8 @@ public class Member {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setUsername(String name) {
+        this.username = name;
     }
 
     public Integer getAge() {
